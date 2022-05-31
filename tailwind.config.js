0d7e3a8944ba24ccdf2e8e4917e2d82528da53c8
wahyunanda1 <xlwahyu20@gmail.com>
2022-05-31 13:05:58 +0800

    Portfolio Wahyu Nahda Putra Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#f97316',
+        secondary: '#1e293b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
