913513a6c2f66e83741ed1224fe8f018bf6b77fb
wahyunanda1 <xlwahyu20@gmail.com>
2022-06-01 15:55:07 +0800

    Sdkit lgi fix

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 };
 
@@ -19,3 +24,21 @@ hamburger.addEventListener('click', function () {
     navMenu.classList.toggle('hidden');
 }
 );
+
+window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+    } else {
+        html.classList.remove('dark');
+    }
+})
